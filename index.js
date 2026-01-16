bbfb0539509fc6fbb5cba82b07635aa39840dd30
wintercg <79108553+wintercg@users.noreply.github.com>
2026-01-16 22:57:07 +0530

    Refactor index.js to include functions and documentation

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-<script> poc by @Gopal_ethical</script>
+/**
+ * twilio-node
+ * poc for bug bounty 
+ */
 
+function hello(name = 'World') {
+  return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  hello,
+  add
+};
